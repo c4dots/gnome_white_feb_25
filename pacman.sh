dbb380afead58a60826cc4423bef065f18f62413
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-02-22 10:02:37 +0100

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -114,6 +114,8 @@ else
 fi
 
 if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo ">> ZShell is already installed. Skipping installation."
+else
     echo ">> Installing ZShell..."
     yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sed -i 's/ZSH_THEME=".*"/ZSH_THEME="jonathan"/' ~/.zshrc
@@ -121,8 +123,6 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/binding "'<Super>t'"
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/command "'gnome-terminal -- zsh'"
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/name "'terminal'"
-else
-    echo ">> ZShell is already installed. Skipping installation."
 fi
 #################### PACKAGES ####################
 
